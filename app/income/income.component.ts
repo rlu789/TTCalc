e434afc41a8d813db88e914a4fdfe8da2581fb7e
DESKTOP-K1IMU4P\Richard <richard.lu@griffithuni.edu.au>
2018-06-01 10:32:38 +1000

    for each section

--- app/income/income.component.ts
+++ app/income/income.component.ts
@@ -6,16 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./income.component.css']
 })
 export class IncomeComponent implements OnInit {
-  income = {
-    SWA : {
-      gross: null,
-      withheld: null
-    }
-  }
+  income = [
+    {
+      section: "SWA",
+      fields: [
+        { name: "Gross Income", value: null },
+        { name: "Tax Withheld", value: null },
+      ]
+    },
+    {
+      section: "Interest",
+      fields: [
+        { name: "Gross Interest", value: null },
+      ]
+    },
+  ]
 
   constructor() {
     if (localStorage.getItem('income'))
       this.income = JSON.parse(localStorage.getItem('income'));
+    else
+      localStorage.setItem('income', JSON.stringify(this.income));
   }
 
   ngOnInit() {
