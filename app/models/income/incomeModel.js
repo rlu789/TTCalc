3fe61112efb50f00dcef37a44d88e86ffdb583bd
rlu789 <rlu789@hotmail.com>
2018-06-01 16:00:01 +1000

    income calcs array

--- app/models/income/incomeModel.js
+++ app/models/income/incomeModel.js
@@ -7,40 +7,38 @@ var income = {
             { name: "Gross Income", value: null },
             { name: "Tax Withheld", value: null },
         ],
-        calcs: [
-            { function: '+' },
-            { function: '-' },
-        ]
     },
     Interest: {
         section: "Interest",
         fields: [
             { name: "Gross Interest", value: null },
-        ],
-        calcs: [
-            { function: '+' },
         ]
     },
 };
+var incomeCalcs = [
+    { key: "SWA", calcs: [{ function: '+' }, { function: '-' }] },
+    { key: "Interest", calcs: [{ function: '+' }] },
+];
 function initIncome() {
     return income;
 }
 exports.initIncome = initIncome;
 function calcIncomeTotal() {
     var totalIncome = 0, totalWithheld = 0;
-    //for (var i = 0; i < income.length; i++) {
-    //  for (var j = 0; j < income[i].fields.length; j++) {
-    //    // TODO basic error check to see if theres actually a functions array
-    //    switch (income[i].calcs[j].function) {
-    //      case '+':
-    //        totalIncome += income[i].fields[j].value
-    //        break;
-    //      case '-':
-    //        totalWithheld += income[i].fields[j].value
-    //        break;
-    //    }
-    //  }
-    //}
+    for (var i = 0; i < incomeCalcs.length; i++) {
+        for (var j = 0; j < incomeCalcs[i].calcs.length; j++) {
+            // TODO basic error check to see if theres actually a functions array
+            // TODO this might be the ugliest think ive ever seen
+            switch (incomeCalcs[i].calcs[j].function) {
+                case '+':
+                    totalIncome += income[incomeCalcs[i].key].fields[j].value;
+                    break;
+                case '-':
+                    totalWithheld += income[incomeCalcs[i].key].fields[j].value;
+                    break;
+            }
+        }
+    }
     return { income: totalIncome, withheld: totalWithheld, final: totalIncome - totalWithheld };
 }
 exports.calcIncomeTotal = calcIncomeTotal;
